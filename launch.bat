9e8f92f79f657c0f69818e320d46599dfbb266e2
AgentKL <75439661+SpyKL@users.noreply.github.com>
2025-01-29 16:46:20 +0100

    Change launch.bat to resolve path problem
    Yeah I make change 3 year after xD

--- launch.bat
+++ launch.bat
@@ -1,2 +1,15 @@
-xdg-open http://127.0.0.1:5000
-python \MonnayeurCode/app.py
+@echo off
+setlocal
+
+:: Définir le chemin du script en fonction de l'emplacement du fichier batch
+set "SCRIPT_DIR=%~dp0"
+set "APP_PATH=%SCRIPT_DIR%MonnayeurCode\app.py"
+
+:: Ouvrir l'URL dans le navigateur
+start "" http://127.0.0.1:5000
+
+:: Lancer le script Python
+python "%APP_PATH%"
+
+:: Pause pour garder la console ouverte
+pause
